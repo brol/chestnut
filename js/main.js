cc211fefbabe6225f9fc185112316e9f54e5ef2a
brol <brol31@gmail.com>
2016-08-12 23:46:47 +0200

    1.3.3

--- js/main.js
+++ js/main.js
@@ -1,55 +1,56 @@
 $(function() {
 
-    // Slider - Cycle Plugin
+	// Slider - Cycle Plugin
     $('#slider').cycle({
-        fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-        timeout: 8000
-    });
+		fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+		timeout: 8000
+	});
 
-    // Jquery - Autocomplete Plugin
-    $('#q,#query').autocomplete({
-        minLength: 2,
-        source: website.ajaxsearch,
-        select: function(event, ui) {
-            $(this).val(ui.item.label);
-            $(this).closest("form").submit();
-        }
-    });
 
-    // Jquery - Tabs
-    if($("#tabs").length > 0) {
-        $("#tabs").tabs({ show: { effect: "fade", duration: 800 } });
+	// Jquery - Autocomplete Plugin
+	$('#q,#query').autocomplete({
+		minLength: 2,
+		source: website.ajaxsearch,
+		select: function(event, ui) {
+			$(this).val(ui.item.label);
+			$(this).closest("form").submit();
+		}
+	});
 
-        if(document.location.hash!='') {
-                tabSelect = document.location.hash.substr(1,document.location.hash.length);
-                if(tabSelect.match(/^comment/) != null) {
-                  $("#tabs").tabs('enable','#comment');
-                }
+	// Jquery - Tabs
+	if($("#tabs").length > 0) {
+		$("#tabs").tabs({ fx: { opacity: 'toggle' } });
 
-                if(tabSelect.match(/^c[1-9]/) != null) {
-                  $("#tabs").tabs('enable','#comment');
-                  $("#tabs").on('tabsactivate', function(event, ui) {
-                    var $target = $(document.location.hash);
-                    var targetOffset = $target.offset().top;
-                    $("html,body").animate({scrollTop: targetOffset-100}, 1000);
-                  });
-                }
+		if(document.location.hash!='') {
+				tabSelect = document.location.hash.substr(1,document.location.hash.length);
+				if(tabSelect.match(/^comment/) != null) {
+				  $("#tabs").tabs('select','#comment');
+				}
 
-                if(tabSelect.match(/^pr/) != null) {
-                  $("#tabs").tabs('enable','#comment');
-                  $("#tabs").on('tabsactivate', function(event, ui) {
-                    var $target = $(document.location.hash);
-                    var targetOffset = $target.offset().top;
-                    $("html,body").animate({scrollTop: targetOffset-100}, 1000);
-                  });
-                }
-        }
+				if(tabSelect.match(/^c[1-9]/) != null) {
+				  $("#tabs").tabs('select','#comment');
+				  $("#tabs").bind("tabsshow", function(event, ui) {
+					var $target = $(document.location.hash);
+					var targetOffset = $target.offset().top;
+					$("html,body").animate({scrollTop: targetOffset-100}, 1000);
+				  });
+				}
 
-        $("#linktocomment a").click(function() {
-            $("#tabs").tabs('enable','#comment');
-            var $target = $(document.location.hash);
-            $("html,body").animate({scrollTop: $target-100}, 1000);
-        });
-    }
+				if(tabSelect.match(/^pr/) != null) {
+				  $("#tabs").tabs('select','#comment');
+				  $("#tabs").bind("tabsshow", function(event, ui) {
+					var $target = $(document.location.hash);
+					var targetOffset = $target.offset().top;
+					$("html,body").animate({scrollTop: targetOffset-100}, 1000);
+				  });
+				}
+		}
+
+		$("#linktocomment a").click(function() {
+			$("#tabs").tabs('select','#comment');
+			var $target = $(document.location.hash);
+			$("html,body").animate({scrollTop: $target-100}, 1000);
+		});
+	}
 
 });
